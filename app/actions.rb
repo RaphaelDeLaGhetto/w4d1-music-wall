8dd08422f0cba43b87e6ba567ff1dc91241688b1
Daniel Bidulock <daniel@capitolhill.ca>
2016-01-14 15:53:51 -0500

    Get basic actions and views

--- app/actions.rb
+++ app/actions.rb
@@ -2,3 +2,31 @@
 get '/' do
   erb :index
 end
+
+get '/tracks' do
+  @tracks = Track.all
+  erb :'tracks/index'
+end
+
+get '/tracks/new' do
+  @track = Track.new
+  erb :'tracks/new'
+end
+
+get '/tracks/:id' do
+  @track = Track.find params[:id]
+  erb :'tracks/show'
+end
+
+post '/tracks' do
+  @track = Track.new(
+              song_title: params[:song_title],
+              url: params[:url],
+              author:  params[:author]
+            )
+  if @track.save
+    redirect '/tracks'
+  else
+    erb :'tracks/new'
+  end
+end
